480296c794009d67b80e5ef9c08e62d2616ea0a0
MuhammadSalman009-tech <salmansumra009@gmail.com>
2021-03-18 19:01:15 +0500

    reaction-picker final setup

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,38 @@
   <h1>app component</h1>
   <br>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from './components/block'
-
+import Results from './components/results.vue'
 export default {
   name: 'App',
   components: {
-  Block
+  Block,
+  Results
   },
   data(){
     return{
       isPlaying:false,
-      delay:null
+      delay:null,
+      score:null,
+      showResults:false
     }
   },
   methods:{
     start(){
-      this.delay=2000+Math.random()*5000
-      this.isPlaying=true
+      this.delay=2000+Math.random()*5000,
+      this.isPlaying=true,
+      this.showResults=false,
       console.log(this.delay)
+    },
+    endGame(reactionTime){
+      this.score=reactionTime,
+      this.isPlaying=false,
+      this.showResults=true
     }
   }
 }
@@ -37,5 +47,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  background-color: seagreen;
+  color: #fff;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+}
+button[disabled]{
+  cursor:not-allowed;
+  opacity: 0.6;
 }
 </style>
---
+++ src/components/results.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="results">
+    <p>Reaction Time: {{score}}ms</p>
+    <h2>{{rank}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            rank:null
+        }
+    },
+    props:["score"],
+    mounted(){
+    if(this.score<250){
+        this.rank="1st Rank"
+    }else if(this.score<500){
+        this.rank="2nd Rank"
+    }else{
+        this.rank="3rd Rank"
+    }
+}
+}
+</script>
+
+<style>
+.results h2{
+    color: seagreen;
+}
+</style>
